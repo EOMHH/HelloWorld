6889621dd59ff1b76846540286edab236bce63f6
fannieming <fannieming@tencent.com>
2021-08-06 17:01:41 +0800

    git回滚测试

---
+++ src/components/form.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <el-card shadow="always" :body-style="{ padding: '20px' }">
+            <div slot="header">
+                <span>查询</span>
+            </div>
+        </el-card>
+        <div>cnsjkavdsjkavbdj</div>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      FormData: []
+    }
+  }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button type="primary" size="default" @click="goto('/about/child')">跳转form</el-button>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    goto (url) {
+      this.$router.push(
+        {
+          path: url
+        })
+    }
+  }
+}
+</script>
